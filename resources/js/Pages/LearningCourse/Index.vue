a98d6aede60c75246545440044133838220331cc
Mark Villudo <markanthony.villudo@gmail.com>
2024-04-20 17:01:52 +0800

    .

--- resources/js/Pages/LearningCourse/Index.vue
+++ resources/js/Pages/LearningCourse/Index.vue
@@ -6,7 +6,7 @@
       <div class="py-12 mx-2 py-2">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-white">
           <div class="py-4 text-right">
-            <Link class="inline-flex items-center px-6 py-4 bg-gray-800 text-white font-bold rounded-lg shadow hover:bg-gray-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" href="learning-modules/create">Add New Learning Module</Link>
+            <Link class="border-b inline-flex items-center px-6 py-4 bg-gray-800 text-white font-bold rounded-lg shadow hover:bg-gray-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" href="learning-modules/create">Add New Learning Module</Link>
           </div>
           <template>
             {{  message }}
@@ -19,29 +19,28 @@
               <table class="border-separate border border-slate-500">
                 <tbody v-for="sy in schoolYear" :key="sy.id">
                   <tr>
-                    <th class="text-xl font-medium cursor-pointer" scope="row">
+                    <td colspan="3" class="text-xl font-medium cursor-pointer border-y pl-2" scope="row">
                       {{ sy.name }}
-                    </th>
+                    </td>
                   </tr>
                   <tr v-for="learning_course in sy.learning_courses" :key="learning_course.id" class="mx-4">
-                    <td> </td>
-                    <td>
+                    <td colspan="2" class="border-r pl-6">
                       <a href="#" class="font-bold">{{ learning_course.title }}</a>
                         <p class="text-sm">
                           {{ learning_course.description }}
                         </p>
                     </td>
-                      <td class="grid grid-cols-2 gap-2">
-                        <div class="flex space-x-2">
-                          <button @click="deleteLearningCourse(learning_course.id)" class="inline-flex items-center px-2 py-1 my-6 text-sm font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                            <i class="fas fa-trash-alt mr-1"></i>
-                            Delete
-                          </button>
-                          <Link :href="`learning-modules/${learning_course.id}/edit`" class="inline-flex items-center px-2 py-1 my-6 text-sm font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                            <i class="fas fa-edit mr-1"></i>
-                            Edit
-                          </Link>
-                        </div>
+                    <td class="grid grid-cols-1 gap-2 text-center">
+                      <div class="sm:flex flex px-4">
+                        <button @click="deleteLearningCourse(learning_course.id)" class="sm:flex-auto items-center px-4 py-1 my-4 text-sm font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                          <i class="fas fa-trash-alt mr-1"></i>
+                          Delete
+                        </button>
+                        <Link :href="`learning-modules/${learning_course.id}/edit`" class="sm:flex-auto items-center px-4 py-1 my-4 text-sm font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                          <i class="fas fa-edit mr-1"></i>
+                          Edit
+                        </Link>
+                      </div>
                     </td>
                   </tr>
                 </tbody>
