8f7890128dcf818b8c81de643723772ec3aac239
Mark Villudo <markanthony.villudo@gmail.com>
2024-05-17 15:48:03 +0800

    update table width

--- resources/js/Pages/LearningCourse/Index.vue
+++ resources/js/Pages/LearningCourse/Index.vue
@@ -3,56 +3,54 @@
         <template #header>
           <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Learning Subject per Student Year</h2>
         </template>
-      <div class="py-12 mx-2 py-2">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-white">
           <div class="py-4 text-right">
             <Link class="border-b inline-flex items-center px-6 py-4 bg-gray-800 text-white font-bold rounded-lg shadow hover:bg-gray-200 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500" href="learning-courses/create">Add New Subject</Link>
           </div>
-          <div class="py-12" v-if="message">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-              <template >
-                <div class="alert alert-success">
-                  {{ message }}
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <template >
+              <div class="alert alert-success">
+                {{ message }}
+              </div>
+            </template>
+          </div>
+          <div class="flex flex-wrap mt-4 px-12">
+            <div class="w-full xl:w-12/12 mb-12 xl:mb-0">
+                <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                    <div class="dark:bg-white text-black dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <table class="border-separate border border-slate-500 w-full">
+                          <tbody v-for="sy in schoolYear" :key="sy.id">
+                            <tr>
+                              <td colspan="3" class="text-xl font-medium cursor-pointer border-y pl-2" scope="row">
+                                {{ sy.name }}
+                              </td>
+                            </tr>
+                            <tr v-for="learning_course in sy.learning_courses" :key="learning_course.id" class="mx-4">
+                              <td colspan="2" class="border-r pl-6">
+                                <a href="#" class="font-bold">{{ learning_course.title }}</a>
+                                  <p class="text-sm">
+                                    {{ learning_course.description }}
+                                  </p>
+                              </td>
+                              <td class="md:grid grid-cols-1 gap-2 text-center flex flex-row px-1 py-1 items-center">
+                                <div class="flex gap-1 flex-col md:flex-row items-center justify-center">
+                                  <button @click="handleDeleteConfirmation(learning_course.id)" class="items-center py-1 w-24 text-sm font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                                    <i class="fas fa-trash-alt mr-1"></i>
+                                    Delete
+                                  </button>
+                                  <Link :href="`learning-courses/${learning_course.id}/edit`" class="items-center py-1 w-24 text-sm font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                    <i class="fas fa-edit mr-1"></i>
+                                    Edit
+                                  </Link>
+                                  
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                    </div>
+                  </div>
                 </div>
-              </template>
             </div>
-          </div>
-          <div class="dark:bg-white text-black dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="container mx-auto px-4 py-8">
-              <table class="border-separate border border-slate-500">
-                <tbody v-for="sy in schoolYear" :key="sy.id">
-                  <tr>
-                    <td colspan="3" class="text-xl font-medium cursor-pointer border-y pl-2" scope="row">
-                      {{ sy.name }}
-                    </td>
-                  </tr>
-                  <tr v-for="learning_course in sy.learning_courses" :key="learning_course.id" class="mx-4">
-                    <td colspan="2" class="border-r pl-6">
-                      <a href="#" class="font-bold">{{ learning_course.title }}</a>
-                        <p class="text-sm">
-                          {{ learning_course.description }}
-                        </p>
-                    </td>
-                    <td class="md:grid grid-cols-1 gap-2 text-center flex flex-row px-1 py-1 items-center">
-                      <div class="flex gap-1 flex-col md:flex-row items-center justify-center">
-                        <button @click="handleDeleteConfirmation(learning_course.id)" class="items-center py-1 w-24 text-sm font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                          <i class="fas fa-trash-alt mr-1"></i>
-                          Delete
-                        </button>
-                        <Link :href="`learning-courses/${learning_course.id}/edit`" class="items-center py-1 w-24 text-sm font-medium text-center text-white rounded-lg bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                          <i class="fas fa-edit mr-1"></i>
-                          Edit
-                        </Link>
-                        
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
     </AuthenticatedLayout>
   </template>
   
