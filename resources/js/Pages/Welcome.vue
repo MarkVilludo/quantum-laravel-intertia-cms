726c0471fef16c71ff4e06226a4100ae6b4dfe95
Mark Villudo <markanthony.villudo@gmail.com>
2024-05-17 15:00:55 +0800

    update welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -21,56 +21,52 @@ defineProps({
 
 <template>
     <Head title="Quantum App" />
-
-    <div
-        class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
-    >
-        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-            <Link
-                v-if="$page.props.auth.user"
-                :href="route('dashboard')"
-                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                >Dashboard</Link
-            >
-            <template v-else>
+        <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg">
+        <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+            <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
                 <Link
-                    :href="route('login')"
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
                     class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Log in</Link
+                    >Dashboard</Link
                 >
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >Log in</Link
+                    >
 
-                <Link
-                    v-if="canRegister"
-                    :href="route('register')"
-                    class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                    >Register</Link
-                >
-            </template>
-        </div>
-
-        <!-- content -->
-        <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0 justify-center">
-            <div class="w-full mx-auto bg-white shadow-md rounded-lg overflow-hidden flex flex-col sm:max-w-md px-6 py-4">
-                <h1 class="text-2xl font-bold text-center text-gray-800 mb-6">
-                    Welcome to the Quantum App CMS!
-                </h1>
-                <div class="text-gray-700 text-center space-y-2">
-                    <p>This Content Management System is designed to help you manage your online learning courses.</p>
-                    <p>From creating and editing course content, it provides everything you need to deliver engaging and effective learning experiences.</p>
-                </div>
-               
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                        >Register</Link
+                    >
+                </template>
             </div>
+            <a href="#" id="docs-card" class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
+                <div class="relative flex items-center gap-6 lg:items-end">
+                    <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
+                        <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
+                            <svg class="size-5 sm:size-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path fill="#FF2D20" d="M23 4a1 1 0 0 0-1.447-.894L12.224 7.77a.5.5 0 0 1-.448 0L2.447 3.106A1 1 0 0 0 1 4v13.382a1.99 1.99 0 0 0 1.105 1.79l9.448 4.728c.14.065.293.1.447.1.154-.005.306-.04.447-.105l9.453-4.724a1.99 1.99 0 0 0 1.1-1.789V4ZM3 6.023a.25.25 0 0 1 .362-.223l7.5 3.75a.251.251 0 0 1 .138.223v11.2a.25.25 0 0 1-.362.224l-7.5-3.75a.25.25 0 0 1-.138-.22V6.023Zm18 11.2a.25.25 0 0 1-.138.224l-7.5 3.75a.249.249 0 0 1-.329-.099.249.249 0 0 1-.033-.12V9.772a.251.251 0 0 1 .138-.224l7.5-3.75a.25.25 0 0 1 .362.224v11.2Z"></path><path fill="#FF2D20" d="m3.55 1.893 8 4.048a1.008 1.008 0 0 0 .9 0l8-4.048a1 1 0 0 0-.9-1.785l-7.322 3.706a.506.506 0 0 1-.452 0L4.454.108a1 1 0 0 0-.9 1.785H3.55Z"></path></svg>
+                        </div>
 
-            <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
-            <div class="text-center text-sm sm:text-start">&nbsp;</div>
+                        <div class="pt-3 sm:pt-5 lg:pt-0">
+                            <h2 class="text-xl font-semibold text-black dark:text-white">Quantum App CMS</h2>
 
-                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-end sm:ms-0">
-                    @ Quantum App 2024
+                            <p class="mt-4 text-sm/relaxed">
+                                This Content Management System is designed to help you manage your online learning courses.
+                                From creating and editing course content, it provides everything you need to deliver engaging and effective learning experiences.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
-        <!-- end content -->
-    </div>
+        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+           @ 2024
+        </footer>
 </template>
 
 <style>
