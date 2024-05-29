5a857296441c054c03a459e2b44bdf7b2a9d61aa
Mark Villudo <markanthony.villudo@gmail.com>
2024-05-29 15:50:24 +0800

    .

--- app/Http/Controllers/ReadModuleController.php
+++ app/Http/Controllers/ReadModuleController.php
@@ -16,9 +16,25 @@ class ReadModuleController extends Controller
     public function readModule(ReadModuleRequest $request)
     {
         //save reading progress of user per course module
-        $newModule = $this->readModule->fill($request->all())->save();
-        $data['message'] = 'Successfully saved read module progress.';
-        $data['success'] = true;
+        // check exist before to save
+
+        $isRead = $this->readModule->where('user_id', request('user_id'))
+                                    ->where('learning_course_id', request('learning_course_id'))
+                                    ->where('module_id', request('module_id'))
+                                    ->where('step', request('step'))
+                                    ->where('level', request('level'))
+                                    ->first();
+
+        if (!$isRead) {
+            $newModule = $this->readModule->fill($request->all())->save();
+            $data['message'] = 'Successfully saved read module progress.';
+            $data['success'] = true;
+        } else {
+            $data['message'] = 'Failed, already saved the module progress';
+            $data['success'] = true;
+        }
+
+       
         return response()->json($data, 200);
     }
 }
